de69b00fceeff5a49864a9170d5a6f84fb1561a0
Raman Nv <rnvnoc@gmail.com>
2017-05-02 14:23:56 +0530

    CIO's add user, add attendance, add mark features added

--- main/views.py
+++ main/views.py
@@ -56,14 +56,20 @@ def cio_view(request):
         q = q.split(' ')
         print q
         if q[0] == "view":
-            regNo = str(q[2])
+            regNo = str(q[2]).upper()
             userset = User.objects.filter(regNo=regNo)
             c = {
             'regNo': regNo,
             'userset': userset[0],
             }
             return render(request, "user_view.html", c)
-
+        if q[0] == "add":
+            if q[1] == "user":
+                return redirect(user_create)
+            elif q[1] == "attendance":
+                return redirect(attendance_create)
+            elif (q[1] == "marks" or q[1] == "mark"):
+                return redirect(mark_create)
         if q[0] == "generate":
             print "set to generate"
 
